de5764f3b68dd4de2452be97d41831fbdcf2081d
bob0777-code <usofnab79518@gmail.com>
2026-07-05 05:36:36 +0100

    Create config.lua

---
+++ lib/config.lua
@@ -0,0 +1,15 @@
+local Config = {}
+
+Config.title = "Silicon Reach"
+
+Config.refreshRate = 2
+
+Config.topItems = 10
+
+Config.rotatePages = true
+
+Config.pageTime = 10
+
+Config.monitorScale = 0.5
+
+return Config
