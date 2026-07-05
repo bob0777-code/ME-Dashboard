607dc2d5e69a2210626fcd56133a2a093c619146
bob0777-code <usofnab79518@gmail.com>
2026-07-05 17:06:31 +0100

    Update config.lua

--- lib/config.lua
+++ lib/config.lua
@@ -1,8 +1,19 @@
 local Config={}
 
-Config.title="Silicon Reach ME Dashboard"
 Config.refreshRate=15
 Config.textScale=0.5
 Config.topItems=10
+
+function Config.getKingdomName()
+ local label=os.getComputerLabel()
+ if label and label~="" then
+  return label
+ end
+ return "Unnamed Kingdom"
+end
+
+function Config.getTitle()
+ return "Kingdom of "..Config.getKingdomName()
+end
 
 return Config
