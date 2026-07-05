66c8c7ac1bdf63a8f92e65ad8a33420ed9220b4b
bob0777-code <usofnab79518@gmail.com>
2026-07-05 16:19:29 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,27 +1,15 @@
 local Loader={}
-
 local cache={}
-
 function Loader.load(path)
-
-    if cache[path] then
-        return cache[path]
-    end
-
-    local filename=path:gsub("%.","/")..".lua"
-
-    local fn,err=loadfile(filename)
-
-    if not fn then
-        error(err)
-    end
-
-    local module=fn()
-
-    cache[path]=module
-
-    return module
-
+ if cache[path] then return cache[path] end
+ local file=path:gsub("%.","/")..".lua"
+ local fn,err=loadfile(file)
+ if not fn then error("Loader failed: "..tostring(err)) end
+ local module=fn()
+ cache[path]=module
+ return module
 end
-
+function Loader.clear()
+ cache={}
+end
 return Loader
