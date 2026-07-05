0199dc3f4283a0d57fc4a7c05c3063a9cf585c75
bob0777-code <usofnab79518@gmail.com>
2026-07-05 05:43:47 +0100

    Update theme.lua

--- lib/theme.lua
+++ lib/theme.lua
@@ -1,21 +1,14 @@
-local Theme = {}
+local Theme={}
 
-Theme.background = colors.black
+Theme.background=colors.black
+Theme.text=colors.white
+Theme.title=colors.cyan
+Theme.border=colors.gray
 
-Theme.text = colors.white
+Theme.good=colors.lime
+Theme.warning=colors.yellow
+Theme.bad=colors.red
 
-Theme.title = colors.cyan
-
-Theme.border = colors.gray
-
-Theme.good = colors.lime
-
-Theme.warning = colors.yellow
-
-Theme.bad = colors.red
-
-Theme.highlight = colors.lightBlue
-
-Theme.shadow = colors.lightGray
+Theme.highlight=colors.lightBlue
 
 return Theme
