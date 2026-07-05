6451b9684ff73022acb2cc03ca3b98c459a027da
bob0777-code <usofnab79518@gmail.com>
2026-07-05 05:36:12 +0100

    Create theme.lua

---
+++ lib/theme.lua
@@ -0,0 +1,21 @@
+local Theme = {}
+
+Theme.background = colors.black
+
+Theme.text = colors.white
+
+Theme.title = colors.cyan
+
+Theme.border = colors.gray
+
+Theme.good = colors.lime
+
+Theme.warning = colors.yellow
+
+Theme.bad = colors.red
+
+Theme.highlight = colors.lightBlue
+
+Theme.shadow = colors.lightGray
+
+return Theme
