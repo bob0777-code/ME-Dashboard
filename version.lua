d42ce4d63b6f3e7c185213a870cf670cfbfc6977
bob0777-code <usofnab79518@gmail.com>
2026-07-05 21:19:16 +0100

    Update version.lua

--- version.lua
+++ version.lua
@@ -1,9 +1,5 @@
-local Version = {}
-
-Version.name = "ME Dashboard"
-
-Version.version = "0.0.1"
-
-Version.author = "Ariya + ChatGPT"
-
+local Version={}
+Version.name="ME Dashboard"
+Version.version="0.2.0"
+Version.author="Ariya + ChatGPT"
 return Version
