feed50c31d8a8e810b2c5637be2f1104eff6671c
bob0777-code <usofnab79518@gmail.com>
2026-07-05 16:19:43 +0100

    Update peripherals.lua

--- lib/peripherals.lua
+++ lib/peripherals.lua
@@ -1,30 +1,21 @@
 local Peripherals={}
 
-Peripherals.colony=peripheral.find("colony_integrator")
-Peripherals.monitor=peripheral.find("monitor")
-Peripherals.me=peripheral.find("me_bridge")
-Peripherals.stash=peripheral.find("minecolonies:stash")
-
 function Peripherals.refresh()
-
-    Peripherals.colony=peripheral.find("colony_integrator")
-    Peripherals.monitor=peripheral.find("monitor")
-    Peripherals.me=peripheral.find("me_bridge")
-    Peripherals.stash=peripheral.find("minecolonies:stash")
-
+ Peripherals.colony=peripheral.find("colony_integrator")
+ Peripherals.monitor=peripheral.find("monitor")
+ Peripherals.me=peripheral.find("me_bridge")
+ Peripherals.stash=peripheral.find("minecolonies:stash")
 end
 
 function Peripherals.verify()
-
-    local status={}
-
-    status.me=Peripherals.me~=nil
-    status.monitor=Peripherals.monitor~=nil
-    status.colony=Peripherals.colony~=nil
-    status.stash=Peripherals.stash~=nil
-
-    return status
-
+ return {
+  colony=Peripherals.colony~=nil,
+  monitor=Peripherals.monitor~=nil,
+  me=Peripherals.me~=nil,
+  stash=Peripherals.stash~=nil
+ }
 end
+
+Peripherals.refresh()
 
 return Peripherals
